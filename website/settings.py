5c2ff6ba14b65f943602d32bf71d3d2f7e37914f
Zammad Khan <97607052+iamzammad@users.noreply.github.com>
2026-03-19 23:33:14 +0500

    fix static settings

--- website/settings.py
+++ website/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = '/home/izzanaseer/ms_enterprise/staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'myapp' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
